4b4d45f65659fe1217ce91e399b777819452b276
Daynor Montoya Chura <montoya.chura.daynor@gmail.com>
2026-05-09 19:55:00 -0400

    deploy server active

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,47 @@
-import { useState } from 'react'
-import { useWebSocket } from './hooks/useWebSocket'
-import ChatBox from './components/ChatBox'
-import UsernameModal from './components/UsernameModal'
+import { useState } from "react";
+import { useWebSocket } from "./hooks/useWebSocket";
+import ChatBox from "./components/ChatBox";
+import UsernameModal from "./components/UsernameModal";
 
 function App() {
-  const { messages, isConnected, currentUser, setUserName, sendChatMessage } = useWebSocket('ws://127.0.0.1:3001')
+  const { messages, isConnected, currentUser, setUserName, sendChatMessage } =
+    useWebSocket("https://socketflow-sc.onrender.com/");
 
-
-  const [showModal, setShowModal] = useState(true)
+  const [showModal, setShowModal] = useState(true);
 
   const handleConfirmName = (name: string) => {
-    setUserName(name)
-    setShowModal(false)
-  }
+    setUserName(name);
+    setShowModal(false);
+  };
 
   return (
     <div className="min-h-screen bg-[#ffff00] flex items-center justify-center p-4 font-mono">
       {showModal && currentUser && (
-        <UsernameModal 
-          onConfirm={handleConfirmName} 
-          currentName={currentUser.name} 
+        <UsernameModal
+          onConfirm={handleConfirmName}
+          currentName={currentUser.name}
         />
       )}
 
       {currentUser && (
-        <ChatBox 
-          messages={messages} 
+        <ChatBox
+          messages={messages}
           currentUserId={currentUser.id}
           onSendMessage={sendChatMessage}
           isConnected={isConnected}
         />
       )}
-      
+
       {!currentUser && (
         <div className="flex flex-col items-center gap-4">
           <div className="w-16 h-16 border-8 border-black border-t-transparent animate-spin"></div>
-          <p className="text-black font-black uppercase text-xl">CONECTANDO AL NODO...</p>
+          <p className="text-black font-black uppercase text-xl">
+            CONECTANDO AL NODO...
+          </p>
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
