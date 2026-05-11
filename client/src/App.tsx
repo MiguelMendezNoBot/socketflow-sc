1256581b835775014db8218acfd6c40c038185cd
ivan-el-Terrible <estradaivan254@gmail.com>
2026-05-11 08:44:37 -0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,3 @@
-// TODO: HU-07 — mostrar UsernameModal al entrar
-// TODO: HU-06 — conectar al WebSocket usando useWebSocket
-// TODO: HU-09 — pasar mensajes al ChatBox
-
-/*function App() {
-  return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <p className="text-gray-400">SocketFlow SC — en construcción</p>
-    </div>
-  )
-}
 
 export default App*/
 import "./index.css";
